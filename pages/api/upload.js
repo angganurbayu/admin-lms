6db5a18ff133e6a76f527e2cb90328328109d02c
Bayu Nur Angga <bayunurangga21@gmail.com>
2023-05-14 19:15:47 +0700

    cors origin allowed credentials

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,3 +1,4 @@
 export default function handler(req, res) {
-    res.status(200).setHeader('Access-Control-Allow-Origin', '*').json({ name: 'Fake Upload Process' });
+    res.status(200).res.header( "Access-Control-Allow-Origin", env.get("ALLOWED_CORS_DOMAINS") );
+    res.header( "Access-Control-Allow-Credentials", true );
 }
